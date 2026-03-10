fcbaed548c58d76516c3b80c23b34d717a5b1334
hezhicheng2002 <herichard88@gmial.com>
2026-03-10 14:25:33 +0800

    test: make arxiv retriever test fully local

--- tests/retriever/test_arxiv_retriever.py
+++ tests/retriever/test_arxiv_retriever.py
@@ -1,21 +1,33 @@
+from types import SimpleNamespace
+
 from zotero_arxiv_daily.retriever.arxiv_retriever import ArxivRetriever
-import feedparser
-import pickle
+
 
 def test_arxiv_retriever(config, monkeypatch):
+    raw_papers = [
+        SimpleNamespace(
+            title="Paper A",
+            authors=[SimpleNamespace(name="Author 1"), SimpleNamespace(name="Author 2")],
+            summary="Abstract A",
+            entry_id="https://arxiv.org/abs/1234.5678",
+            pdf_url="https://arxiv.org/pdf/1234.5678.pdf",
+        ),
+        SimpleNamespace(
+            title="Paper B",
+            authors=[SimpleNamespace(name="Author 3")],
+            summary="Abstract B",
+            entry_id="https://arxiv.org/abs/2345.6789",
+            pdf_url="https://arxiv.org/pdf/2345.6789.pdf",
+        ),
+    ]
 
-    parsed_result = feedparser.parse("tests/retriever/arxiv_rss_example.xml")
-    raw_parser = feedparser.parse
-    def mock_feedparser_parse(url):
-        if url == f"https://rss.arxiv.org/atom/{'+'.join(config.source.arxiv.category)}":
-            return parsed_result
-        return raw_parser(url)
-    monkeypatch.setattr(feedparser, "parse", mock_feedparser_parse)
-    
+    monkeypatch.setattr(ArxivRetriever, "_retrieve_raw_papers", lambda self: raw_papers)
+
     retriever = ArxivRetriever(config)
     papers = retriever.retrieve_papers()
-    parsed_results = [i for i in parsed_result.entries if i.get("arxiv_announce_type","new") == 'new']
-    assert len(papers) == len(parsed_results)
-    paper_titles = [i.title for i in papers]
-    parsed_titles = [i.title for i in parsed_results]
-    assert set(paper_titles) == set(parsed_titles)
+
+    assert len(papers) == len(raw_papers)
+    assert [paper.title for paper in papers] == ["Paper A", "Paper B"]
+    assert papers[0].authors == ["Author 1", "Author 2"]
+    assert papers[1].abstract == "Abstract B"
+    assert all(paper.full_text is None for paper in papers)
